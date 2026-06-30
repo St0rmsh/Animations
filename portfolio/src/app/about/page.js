82265c90a923a94452bca010462f2eb7966cfd0a
Sujit Kumar <sujitku5619@gmail.com>
2026-06-30 08:19:59 +0530

    feat: enhance about page with TextReveal component and create CarouselCard and InfiniteCarousel components

--- portfolio/src/app/about/page.js
+++ portfolio/src/app/about/page.js
@@ -1,8 +1,22 @@
+import TextReveal from '@/components/TextReveal'
 import React from 'react'
 
 const page = () => {
   return (
-    <div className=''> This is  about page</div>
+    <>
+    <main className="flex h-[400vh] flex-col items-center justify-between p-54 w-full"> 
+
+    <div className=''> 
+       <TextReveal className="text-5xl font-bold"> This is  about page  </TextReveal> 
+       </div>
+
+    </main>
+
+
+  
+    </>
+
+ 
   )
 }
 
